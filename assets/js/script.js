86b8bb5b701ee72acb1cd0ee2d7a1957ca27847c
Quincy Maynor <131222776+quincymaynor@users.noreply.github.com>
2023-06-10 11:43:43 -0500

    search results finally display on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,15 +25,11 @@ function getInfo(input){
 }
 
 function printDataToPage(results){
-    // console.log("second log", data)
-    // console.log("second log", results.data.length)
     if (results.data.length === 0) {
       resultsContainer.textContent = 'No search results found.';
       return;
     } else {
         for (var i = 0; i < results.data.length; i++) {
-            // console.log("third log", results);
-            // console.log("fourth log", results.data[i]);
 
             var resultsContainer = document.createElement('div');
             var resultCard = document.createElement('div');
@@ -41,9 +37,6 @@ function printDataToPage(results){
             var resultTitle = document.createElement('h3');
             var resultArtist = document.createElement('p');
             var addBtn = document.createElement('button');
-
-            resultsEl.append(resultsContainer);
-            resultsContainer.append(resultCard);
 
             var albumCover = results.data[i].album.cover_medium;
             resultImg.setAttribute('src', albumCover);
@@ -59,32 +52,12 @@ function printDataToPage(results){
 
             resultCard.append(addBtn);
 
-            console.log("this is the cover", resultImg);
-            console.log("this is the song", resultTitle);
-            console.log("this is the artist", resultArtist);
+            // console.log("this is the cover", resultImg);
+            // console.log("this is the song", resultTitle);
+            // console.log("this is the artist", resultArtist);
 
-            // var albumTitle = results.data[i].album.title;
-
-            // console.log(artistName)
-            
-            //resultCard.classList.add('');
-            
-
-            //resultImg.classList = '';
-            // resultImg.src = albumCover;
-            
-            //resultTitle.classList = '';
-           
-            // resultArtist.classList = '';
-            // resultArtist.textContent = artistName;
-            
-            // addBtn.classList = '';
-            
-            // resultsContainer.appendChild(resultCard);
-            // resultCard.appendChild(resultImg);
-            // resultCard.appendChild(resultTitle);
-            // resultsContainer.append(resultArtist);
-            // resultCard.appendChild(addBtn);
+            resultsContainer.append(resultCard);
+            resultsEl.append(resultsContainer);
           }
     } 
 };
@@ -103,14 +76,7 @@ addPlaylistButtonEl.on('click', function(){
     addPlaylist(userInput)
 })
 
-// fetch data based on search input
-// populate data on page
 
 // .playlist-card
 // .playlist-img
 // .playlist-title
-
-// .result-card
-// .result-img
-// .result-title
-// .add-to-playlist
