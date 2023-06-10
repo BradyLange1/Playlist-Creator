7f79eea8b2247a3a0222598bd77f80e7bb290cbc
Quincy Maynor <131222776+quincymaynor@users.noreply.github.com>
2023-06-10 10:23:40 -0500

    in the process of fixing results printing function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,41 +19,52 @@ function getInfo(input){
             return response.json()
         })
         .then(function(data){
-            console.log(data)
-            printDataToPage(data)
+            console.log(data);
+            printDataToPage(data);
         })
 }
 
 function printDataToPage(data){
-    if (data.length === 0) {
+    console.log("second log", data)
+    console.log("second log", data.data.length)
+    if (data.data.length === 0) {
       resultsContainer.textContent = 'No search results found.';
       return;
     } else {
-        for (var i = 0; i < data.length; i++) {
-            var artistName = data[i].artist.name;
-          //var albumTitle = data[i].album.title;
-            var songTitle = data[i].title;
-            var albumCover = data[i].album.cover_medium;
+        for (var i = 0; i < data.data.length; i++) {
+            console.log("third log", data);
+            console.log("fourth log", data.data[0]);
+
+            var resultArtist = document.createElement('p');
+            resultArtist.textContent = data.data[i].artist.name;
+            resultsContainer.append(resultArtist)
+            console.log("these are the artists", resultArtist)
+        //     var albumTitle = data[i].album.title;
+        //     var songTitle = data[i].title;
+        //     var albumCover = data[i].album.cover_medium;
+
+            // console.log(artistName)
             
-            var resultCard = document.createElement('div');resultCard.classList = '';
+            // var resultCard = document.createElement('div');resultCard.classList.add('');
             
-            var resultImg = document.createElement('img');resultImg.classList = '';
-            resultImg.src = albumCover;
+            // var resultImg = document.createElement('img');resultImg.classList = '';
+            // resultImg.src = albumCover;
             
-            var resultTitle = document.createElement('h3');resultTitle.classList = '';
-            resultTitle.textContent = songTitle;
+            // var resultTitle = document.createElement('h3');resultTitle.classList = '';
+            // resultTitle.textContent = songTitle;
             
-            var resultArtist = document.createElement('p');resultArtist.classList = '';
-            resultArtist.textContent = artistName;
+           
+            // resultArtist.classList = '';
+            // resultArtist.textContent = artistName;
             
             var addBtn = document.createElement('button');
             addBtn.classList = '';
             
-            resultsContainer.appendChild(resultCard);
-            resultCard.appendChild(resultImg);
-            resultCard.appendChild(resultTitle);
-            resultCard.appendChild(resultArtist);
-            resultCard.appendChild(addBtn);
+            // resultsContainer.appendChild(resultCard);
+            // resultCard.appendChild(resultImg);
+            // resultCard.appendChild(resultTitle);
+            // resultsContainer.append(resultArtist);
+            // resultCard.appendChild(addBtn);
           }
     } 
 };
