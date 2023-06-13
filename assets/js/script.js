e3e3ea3c7919bf4c1f6250a5bbc528f31249fb1d
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-06-12 19:13:42 -0500

    added entry validation feature to search

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,29 @@ var addPlaylistButtonEl = $('#add-playlist-button');
 var userInputPlaylistEl = $('#user-input-playlist');
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '55b71cba74msh84c291f4e9de668p126f59jsn31a18769a989',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '55b71cba74msh84c291f4e9de668p126f59jsn31a18769a989',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
 };
 
-function getInfo(input){
+function getInfo(input) {
     const artistUrl = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=' + input;
     fetch(artistUrl, options)
-        .then(function(response){
+        .then(function (response) {
             return response.json()
         })
-        .then(function(data){
+        .then(function (data) {
             console.log(data);
             printDataToPage(data);
         })
 }
 
-function printDataToPage(results){
+function printDataToPage(results) {
     if (results.data.length === 0) {
-      resultsContainer.textContent = 'No search results found.';
-      return;
+        resultsContainer.textContent = 'No search results found.';
+        return;
     } else {
         for (var i = 0; i < results.data.length; i++) {
 
@@ -64,52 +64,60 @@ function printDataToPage(results){
             // console.log("this is the cover", resultImg);
             // console.log("this is the song", resultTitle);
             // console.log("this is the artist", resultArtist);
-          }
-    } 
+        }
+    }
 };
 
-function addPlaylist(input){
+function addPlaylist(input) {
     $("#playlist-form").append("<button class = user-playlist>" + input)
 }
 
-searchButtonEl.on('click', function(){
+searchButtonEl.on('click', function () {
     var userInput = userInputEl.val()
     getInfo(userInput)
 })
 
-addPlaylistButtonEl.on('click', function(){
-    var userInput = userInputPlaylistEl.val()
-    addPlaylist(userInput)
-})
+// Entry validation 
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
 
-<<<<<<< HEAD
+addPlaylistButtonEl.on('click', function () {
+        var userInput = userInputPlaylistEl.val()
+        addPlaylist(userInput)
+    })
+
 
 
 // Get the input field
 
 
 // Execute a function when the user presses a key on the keyboard
-userInputEl.on("keypress", function(event) {
-    
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    console.log("Enter for search!", event)
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    searchButtonEl.click();
-  }
+userInputEl.on("keypress", function (event) {
+
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        console.log("Enter for search!", event)
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        searchButtonEl.click();
+    }
 });
-searchButtonEl.on('click', function(){
-    var userInput = userInputEl.val()
-    getInfo(userInput)
-})
+searchButtonEl.on('click', function () {
+        var userInput = userInputEl.val()
+        getInfo(userInput)
+    })
 
 
-// fetch data based on search input
-// populate data on page
-=======
->>>>>>> 384dc49be13495a5f4d37d8e781334d76890019c
+    // fetch data based on search input
+    // populate data on page
+   
+    
 
 // .playlist-card
 // .playlist-img
